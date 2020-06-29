5c4dd66fb1ac161973226ec22f1c57efd9554030
Jose Chavez <jose.chavezfl02@gmail.com>
2020-06-28 20:25:58 -0500

    crear una sesion

---
+++ vista/creasesion_vista.php
@@ -0,0 +1,8 @@
+
+    <form action="<?php $_SERVER['PHP_SELF']?>" method="post">
+        <input type="text" placeholder="nombre" name="name"/>
+        <input type="password" placeholder="password" name="password"/>
+        <input type="email" placeholder="gmail" name="gmail"/>
+        <button class="btn btn-success" name="boton_inicia">enviar</button>
+    </form>
+
