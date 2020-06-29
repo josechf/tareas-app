b0295c3bf9162f15bf2746741e2eba71f0d40871
Jose Chavez <jose.chavezfl02@gmail.com>
2020-06-28 20:31:58 -0500

    formulario de registro login

---
+++ vista/registrar_vista.php
@@ -0,0 +1,5 @@
+<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+  <input type="text" name="nombre" placeholder="nombre" />
+  <input type="password" name="password" placeholder="password" />
+  <button class="btn btn-outline-dark" name="boton">login</button>
+</form>
